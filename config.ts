d424449aeb52ea52327d2683f4a99918fda6eaa6
Michael Lau <michaellau@innodee.com>
2019-11-19 12:20:16 +0800

    try fixing login

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
 import { AUTH0_CLIENT_ID, AUTH0_DOMAIN, AUTHO_SCOPE, AZURE_ACCOUNT_NAME } from 'react-native-dotenv';
 export default {
-  AUTH0_CLIENT_ID,
-  AUTH0_DOMAIN,
-  AUTHO_SCOPE,
-  AZURE_ACCOUNT_NAME,
+  AZURE_ACCOUNT_NAME: "innodeedevappdocs",
+  AUTH0_CLIENT_ID: "2nEjiGOaob2AvYL1EXbZy9xP4dJ2QixW",
+  AUTH0_DOMAIN: "innodee-devportal.auth0.com",
+  AUTHO_SCOPE: "openid offline_access profile email",
 };
