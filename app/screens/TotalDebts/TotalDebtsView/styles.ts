82eeadb9652aa41e83b79cbfd3a537558d5a781c
miclau13 <miclau2004@gmail.com>
2020-02-11 14:13:56 +0800

    update total debts

--- app/screens/TotalDebts/TotalDebtsView/styles.ts
+++ app/screens/TotalDebts/TotalDebtsView/styles.ts
@@ -2,13 +2,6 @@ import { StyleSheet } from 'react-native';
 import { screenWidth } from '../../../styles';
 
 const styles = StyleSheet.create({
-  autocompleteTextInput: {
-    backgroundColor: 'white',
-    fontSize: 20,
-    marginBottom: 10,
-    marginTop: 10,
-    padding: 10,
-  },
   container: {
     alignItems: "center",
     flex: 1,
