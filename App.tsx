f2d65bcfdf294950168b02c1c1ff0b2d79ed3d25
Michael Lau <michaellau@innodee.com>
2020-03-05 18:43:20 +0800

    fix error

--- App.tsx
+++ App.tsx
@@ -19,9 +19,7 @@ import { LoggedInProvider } from './app/common/LoggedInContext';
         }
       })
     },
-    // uri: 'http://192.168.100.89:3000/graphql',
-    uri: 'http://192.168.0.103:3000/graphql',
-    // uri: 'http://192.168.103.42:3000/graphql',
+    uri: 'https://dev-agent.trudeed.com/graphql'
   });
 
 const App = () => {
